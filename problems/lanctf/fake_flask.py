70337856978eb94ba71edbd5ece629ef00279193
functionxu123 <1229853312@qq.com>
2019-03-20 02:01:56 +0800

    sdf

---
+++ problems/lanctf/fake_flask.py
@@ -0,0 +1,73 @@
+'''
+Created on Mar 20, 2019
+
+@author: sherl
+'''
+
+from flask import Flask
+from urllib.parse import  *
+import hashlib
+from flask import request,make_response,redirect,url_for
+
+SECRET=b'asdfdfg'
+
+
+
+app=Flask(__name__) #创建1个Flask实例
+
+@app.route('/')      #路由系统生成 视图对应url,1. decorator=app.route() 2. decorator(first_flask)
+def hello():    #视图函数
+    return 'Hello World'  #response
+
+@app.route('/sdf')      #路由系统生成 视图对应url,1. decorator=app.route() 2. decorator(first_flask)
+def hello2():    #视图函数
+    return 'error'  #response
+
+@app.route("/admin", methods=["GET"]) 
+def admin_page(): 
+    tep=request.cookies.get("auth")
+    print ('get auth len',len(tep), tep)
+    auth_cookie = unquote_to_bytes(tep) 
+    sig_cookie = unquote_to_bytes(request.cookies.get("sig")) 
+    
+    print (len(auth_cookie),auth_cookie)
+    print (len(sig_cookie), sig_cookie)
+    
+    if auth_cookie is None or sig_cookie is None: 
+        print('one is none')
+        return redirect(url_for("hello")) 
+        
+        
+        
+    if sig_cookie != make_signature(auth_cookie): 
+        print ('not match:',sig_cookie ,make_signature(auth_cookie))
+        resp = make_response(redirect(url_for("hello2"))) 
+        resp.delete_cookie("auth") 
+        resp.delete_cookie("sig") 
+        return resp 
+        
+        
+    cookie_params = {} 
+    for p in auth_cookie.split(b"&"): 
+        key, val = p.split(b"=") 
+        cookie_params[key] = val
+    print('passed')
+    if cookie_params.get(b"role") == b"admin": 
+        return 'FLAG_VALUE' 
+    else: 
+        return redirect(url_for("hello")) 
+    
+def make_signature(value): 
+    temp = SECRET + value 
+    print ('make sig:',len(temp), temp)
+    return hashlib.md5(temp).hexdigest().encode()
+
+
+if __name__ == '__main__':
+    print (make_signature(b'username=test&role=user'))
+    app.run('127.0.0.1', 8012, True)              #启动socket
+    
+    
+    
+    
+    
--- problems/lanctf/underground2.py
+++ problems/lanctf/underground2.py
@@ -10,37 +10,48 @@ from urllib.parse import  *
 import os.path as op
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
-tarurl=r'http://101.201.66.182:8012/admin'
+#tarurl=r'http://101.201.66.182:8012/admin'
+tarurl=r'http://127.0.0.1:8012/admin'
 
 strang_auth='username=test&role=user'
-strang_sig=b'5b8f99d780ec542da912a5a5cf2bc341'
+strang_sig='5b8f99d780ec542da912a5a5cf2bc341'
+
+fake_auth='username=test&role=user'
+fake_sig='484543d217ceab81282abf65fec74721'
 
 
-cookies={"auth": 'username=test&role=user'+'\x80'+'\x00'*25+'\x17'+'\x00'*7,#\role=admin&
-         "sig": b'51c541a5a6a17b50f25ade0192a33dfe'}
+#sec(7) + 23 + 1+ 25+ 1 + 7 + 10 = 74
+#cookies={"auth": 'username=test&role=user'+'%80'+'\x00'*25+'\x1e'+'\x00'*7+'&role=admin',       "sig": '51c541a5a6a17b50f25ade0192a33dfe'}
+cookies={"auth": 'username=test&role=user'+'%80'+'\x00'*25+'\x1e'+'\x00'*7+'&role=admin',       "sig": '1e4714d24c3dcfa82fe64891773b4f11'}
+
+
 print (cookies)
-
+print (len(cookies['auth']))
 
 s=requests.session()
 
 
-'''
+
 r = s.get(tarurl,headers=headers, cookies=cookies) #
 
 
 print (r.cookies)
-print (r.status_code,r.text[:60])
-'''
+print (r.status_code,r.text[:100])
 
-m = hashlib.md5()
-auth_byte=unquote_to_bytes(cookies['auth'])
-print ('after unquote len:',len(auth_byte))
 
+test_quote=cookies['auth']
+auth_byte=unquote_to_bytes(test_quote)
+print ('auth unquote len:',len(auth_byte))
 print ((auth_byte))
 
-m.update(auth_byte)
-print (m.hexdigest())
 
+auth_byte=unquote_to_bytes(cookies['sig'])
+print ('\nsig unquote len:',len(auth_byte))
+print ((auth_byte))
+
+
+
+#print (hashlib.md5(b'username=test&role=user').hexdigest().encode())
 
 
 
